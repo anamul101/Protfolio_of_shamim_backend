7ceb48d6ad8e67b0007ceb9831680f359790eb60
anamul101 <rjanamul01871@gmail.com>
2025-06-29 21:18:44 +0600

    port

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const cors = require("cors");
 app.use(cors());
 app.use("/files", express.static("files"));
 require('dotenv').config();
+const PORT = process.env.PORT || 5000;
 //mongodb connection----------------------------------------------
 // Add validation for DB_URL here ⬇️
 if (!process.env.DB_URL) {
@@ -62,4 +63,7 @@ app.get("/", async (req, res) => {
 
 app.listen(5000, () => {
   console.log("Server Started");
+});
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
